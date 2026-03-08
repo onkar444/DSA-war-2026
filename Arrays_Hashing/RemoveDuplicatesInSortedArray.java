4e1f3a6a4764a5251c36b06aa258a3a88b100b0b
Onkar Raut <66385953+onkar444@users.noreply.github.com>
2026-03-08 12:42:23 +0530

    Implement method to remove duplicates from sorted array

---
+++ Arrays_Hashing/RemoveDuplicatesInSortedArray.java
@@ -0,0 +1,14 @@
+class RemoveDuplicatesInSortedArray {
+    public int removeDuplicates(int[] nums) {
+        int start = 0;
+
+        for(int i = 1; i < nums.length; i++){
+            if(nums[start] != nums[i]){
+                start++;
+                nums[start] = nums[i];
+            }
+        }
+
+        return start + 1;
+    }
+}
